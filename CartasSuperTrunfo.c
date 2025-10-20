2cff28ddde7404ded90c5d8c41bc35955f57e52d
flaviohsfernandes-glitch <flaviohsfernandes@gmail.com>
2025-10-20 13:59:07 +0000

    Criei o printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,12 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
+
+  int populacao = 
+
+
+  printf("Desafio Cartas Super Trunfo!\n");
+  scanf("")
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
