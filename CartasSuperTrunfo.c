45a87d05b5db89628cfaa3d2c8097480b63a386d
flaviohsfernandes-glitch <flaviohsfernandes@gmail.com>
2025-10-22 14:33:16 +0000

    Criei mais printf e scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,23 +6,132 @@
 
 int main() {
 
-  int Numero_de_pontos_turisticos1;
-  int Numero_de_pontos_turisticos2;
-  int populacao1, populacao2;
-  float area1, area2, pib1, pib2;
-  char pais[20];
-  char letra;
   
-  printf("Desafio Cartas Super Trunfo!\n");
+  int Numero_de_pontos_turisticos;
+  int populacao;
+  float area, pib;
+  char pais[1000];
+  char letra;
+  char cidade[1000];
+  char estado[1000];
+  
+  
+  
+  {
+    printf("Desafio Cartas Super Trunfo!\n");
 
   printf("Digite o pais: ");
   scanf("%s", &pais);
   printf("O pais é: %s\n", pais);
+  }
+  {
+  printf("Carta 1:\n");
+
+  printf("Estado: A\n");
+  printf("Digite o estado: ");
+  scanf("%s", &estado);
+  printf("O estado é: %s\n", estado);
+  }
+  {
+  printf("Codigo: A01\n");
 
 
-  printf("Digite a quantidade da populacao1: ");
-  scanf("%d", &populacao1);
-  printf("A quantidade da populacao1 é: %d\n", populacao1);
+  printf("Digite o nome da cidade: ");
+  scanf("%s", &cidade);
+  printf("A cidade é: %s\n", cidade);
+  }
+  {
+  printf("Digite a quantidade da populacao: ");
+  scanf("%d", &populacao);
+  printf("A quantidade da populacao1 é: %d\n", populacao);
+  }
+  {
+  printf("Digite a Area: ");
+  scanf("%f", &area);
+  printf("A area é: %f\n", area);
+  }
+  {
+  printf("Digite o PIB: ");
+  scanf("%f", &pib);
+  printf("O valor do PIB é: %f\n", pib);
+  }
+  {
+  printf("Numero de pontos turisticos: ");
+  scanf("%d", &Numero_de_pontos_turisticos);
+  printf("O numero de pontos turisticos é: %d\n", Numero_de_pontos_turisticos);
+  }
+
+  {
+  printf("Carta 2:\n");
+
+  printf("Estado: A\n");
+  printf("Digite o estado: ");
+  scanf("%s", &estado);
+  printf("O estado é: %s\n", estado);
+
+  printf("Codigo: A02\n");
+  }
+  {
+  printf("Digite o nome da cidade: ");
+  scanf("%s", &cidade);
+  printf("A cidade é: %s\n", cidade);
+  }
+  {
+  printf("Digite a quantidade da populacao: ");
+  scanf("%d", &populacao);
+  printf("A quantidade da populacao1 é: %d\n", populacao);
+  }
+  {
+  printf("Digite a Area: ");
+  scanf("%f", &area);
+  printf("A area é: %f\n", area);
+  }
+  {
+  printf("Digite o PIB: ");
+  scanf("%f", &pib);
+  printf("O valor do PIB é: %f\n", pib);
+  }
+  {
+  printf("Numero de pontos turisticos: ");
+  scanf("%d", &Numero_de_pontos_turisticos);
+  printf("O numero de pontos turisticos é: %d\n", Numero_de_pontos_turisticos);
+  }
+
+  {
+  printf("Carta 3:\n");
+
+  printf("Estado: A\n");
+  printf("Digite o estado: ");
+  scanf("%s", &estado);
+  printf("O estado é: %s\n", estado);
+
+  printf("Codigo: A03\n");
+  }
+  {
+  printf("Digite o nome da cidade: ");
+  scanf("%s", &cidade);
+  printf("A cidade é: %s\n", cidade);
+  }
+  {
+  printf("Digite a quantidade da populacao: ");
+  scanf("%d", &populacao);
+  printf("A quantidade da populacao1 é: %d\n", populacao);
+  }
+  {
+  printf("Digite a Area: ");
+  scanf("%f", &area);
+  printf("A area é: %f\n", area);
+  }
+  {
+  printf("Digite o PIB: ");
+  scanf("%f", &pib);
+  printf("O valor do PIB é: %f\n", pib);
+  }
+  {
+  printf("Numero de pontos turisticos: ");
+  scanf("%d", &Numero_de_pontos_turisticos);
+  printf("O numero de pontos turisticos é: %d\n", Numero_de_pontos_turisticos);
+  }
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
   // Área para entrada de dados
